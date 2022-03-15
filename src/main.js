5a6144c40ffe5449d06cb425e90224859ab7b394
garycarolan <garycarolan@outlook.com>
2022-03-15 14:04:51 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import {createRouter, createWebHistory} from 'vue-router';
 import routes from'./router/routes';
 
+// fake change to see if committing works :)
 let router = createRouter({
     history:createWebHistory(),
     routes:routes,
