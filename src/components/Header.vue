135ef57657400c03acc4696167b39c287a11f7bf
garycarolan <garycarolan@outlook.com>
2022-03-22 18:05:05 +0000

    Made the header neater
    Added stylised navbar, removed 60px top margin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="nav_bar">
-    <ul>
+  <div class="nav">
       <li class = "link" display="block"><router-link to="/">Home</router-link></li>
       <li class = "link" display="block"><router-link to="/register">Register</router-link></li>
       <li class = "link" display="block"><router-link to="/login">Login</router-link></li>
-    </ul>
   </div>
 </template>
 
@@ -15,4 +13,17 @@ export default {
 </script>
 
 <style scoped>
+.nav{
+  background-color:#333;
+  overflow:hidden
+}
+.nav a{
+  float: left;
+  color:#f2f2f2;
+  padding: 14px 16px;
+  text-align: center;
+  font-size:17px;
+  text-decoration: none;
+  margin-right: 5px;
+}
 </style>
