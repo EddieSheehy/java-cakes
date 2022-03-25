07c7b4fb78bfb6985bf417cde640151ab072d45a
dioncollins29 <d.collins29@nuigalway.ie>
2022-03-25 18:31:35 +0000

    Upload Image?

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -38,6 +38,18 @@
         id="beds"
         placeholder="No. Beds"
       />
+      <div class="mb-3 right">
+        <br>
+        <a>Image Upload     </a>
+        <button type="button" @click="click1" class="btn btn-primary">Choose File</button>
+        <input type="file" ref="input1"
+               style="display: none"
+               @change="previewImage" accept="image/>*" >
+      </div>
+      <div v-if="imageData!=null">
+        <img class="preview" height="268" width="356" :src="img1">
+        <br>
+      </div>
     </div>
     <div class="mb-3 right">
       <button type="button" @click="postComment" class="btn btn-primary">
@@ -69,6 +81,9 @@ export default {
       tempValue: "",
       editing: false,
       store,
+      caption: '',
+      img1: '',
+      imageData: null
     };
   },
   components: {
@@ -152,6 +167,46 @@ export default {
         if (result.data == "Deleted document from database") this.getComments(); // To refresh the client
       });
     },
+
+    click1() {
+      this.$refs.input1.click()
+    },
+
+    previewImage(event) {
+      this.uploadValue=0;
+      this.img1=null;
+      this.imageData = event.target.files[0];
+      this.onUpload()
+    },
+
+    onUpload(){
+      this.img1=null;
+      const storageRef=firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+      storageRef.on(`state_changed`,snapshot=>{
+            this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+          }, error=>{console.log(error.message)},
+          ()=>{this.uploadValue=100;
+            storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+              this.img1 =url;
+              console.log(this.img1)
+            });
+          }
+      );
+    },
+    create () {
+      const post = {
+        photo: this.img1,
+        caption: this.caption
+      }
+
+      firebase.database().ref('PhotoGallery').push(post)
+          .then((response) => {
+            console.log(response)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    },
   },
 };
 </script>
@@ -162,5 +217,10 @@ export default {
 }
 .right {
   text-align: right;
+}
+
+a {
+  font-size: 12px;
+  color: gray;
 }
 </style>
